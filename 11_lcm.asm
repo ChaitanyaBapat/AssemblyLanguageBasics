5ce9326ed8e83fa5489849ddb277c9a88f2dfa6a
hello@example.com <hello@example.com>
2017-11-05 15:22:41 +0530

    Find LCM of two numbers

---
+++ 11_lcm.asm
@@ -0,0 +1,38 @@
+data segment
+	num1 dw 0fh
+	num2 dw 06h
+data ends
+
+code segment
+	assume ds:data, cs:code
+start:
+	mov ax,data
+	mov ds,ax
+	int 3
+	mov ax,num1
+	mov bx,num2
+	int 3
+
+while_loop:
+	nop
+	mov cx,bx
+	mov dx,00h
+	int 3
+	div bx
+	int 3
+	mov bx,dx
+	mov ax,cx
+	cmp bx,00h
+	int 3
+	jne while_loop
+	
+	mov cx,ax
+	mov ax,num1
+	mov bx,num2
+	mul bx
+	div cx
+	int 3
+	mov ah,4ch
+	int 21h
+code ends
+end start	
