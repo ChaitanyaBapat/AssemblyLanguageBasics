47ba4bba0846eb05535e5b9b67b14ceecdc96c59
hello@example.com <hello@example.com>
2017-11-05 15:26:37 +0530

    Find cube of the number

---
+++ 15_cube.asm
@@ -0,0 +1,19 @@
+data segment
+	number dw 0020h
+data ends
+code segment
+	assume cs:code,ds:data
+
+start:
+	mov ax,data
+	mov ds,ax
+	mov ax,number
+	mov cx,number
+	mul cx
+	int 3
+	mul cx
+	int 3
+	mov ah,4ch
+	int 21h
+code ends
+end start			
