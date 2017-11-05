82a99d55423b274576d60f8952d32af121c31979
hello@example.com <hello@example.com>
2017-11-05 15:23:40 +0530

    multiply two 16 bit numbers

---
+++ 12_mul_16.asm
@@ -0,0 +1,19 @@
+.model small
+.data
+op1 dw 1000H
+op2 dw 0020H
+.stack
+.code
+main:
+	mov ax,@data
+	mov ds,ax
+	mov ax,0H
+	mov ax,op1
+	mov bx,op2
+	clc
+	mul bx
+	int 3
+	mov ah,4ch
+	int 21h
+end main
+code ends	
