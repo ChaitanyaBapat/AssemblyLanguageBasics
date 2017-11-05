1bb3b5df23ca296359315bfe5c3c0c0e55a3e7d3
hello@example.com <hello@example.com>
2017-11-05 13:38:24 +0530

    Add two 16 bit numbers

---
+++ 7_add_16.asm
@@ -0,0 +1,19 @@
+.model small
+.data
+op1 dw 1111H
+op2 dw 2222H
+.stack
+.code
+main:
+	mov ax,@data
+	mov ds,ax
+	mov ax,0H
+	mov ax,op1
+	mov bx,op2
+	clc
+	add ax,bx
+	int 3
+	mov ah,4ch
+	int 21h
+end main
+code ends	
